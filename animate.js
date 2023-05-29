66552fc6e21cae8c4bf129afb5af6793e96312aa
Karim <110877353+programmerGabut@users.noreply.github.com>
2023-05-29 09:29:14 +0700

    Update animate.js

--- animate.js
+++ animate.js
@@ -2,17 +2,13 @@
 window.addEventListener('load', function () {
   var preloader = document.querySelector('.preloader');
   setTimeout(function () {
-    preloader.style.display = 'none'; // Sembunyikan preloader setelah penundaan
-    // Arahkan pengguna ke halaman utama
+    preloader.style.display = 'none'; 
     window.location.href = 'index.html';
-  }, 3000); // Ganti 3000 dengan jumlah milidetik penundaan yang diinginkan (misalnya 5000 untuk 5 detik)
-});
+  }, 3000); 
 
 const load = gsap.timeline({
   paused: 'true',
 });
-// karena kita sudah defenisikan load seagai objek dari gsap.timeline(), kita dapat memberikan beebrapa metode kepada load
-// seperti .to(), .from(), atau .fromTo()
 
 load.to('#percent , #bar', {
   duration: 0.2,
@@ -24,25 +20,21 @@ load.to('.progress', {
   width: '0%',
 });
 
-// deklarasi id dan width
 var id,
   width = 1;
 
-// dalam funsi 'loading', fungsi setInterval() dipanggil dengan argumen frame dan interval waktu
-// Ini berarti setiap 25 milidetik, fungsi frame() akan dipanggil.
 function loading() {
   id = setInterval(frame, 25);
 }
 
-// PENJELASN DI BAWAH
 function frame() {
   if (width >= 100) {
     clearInterval(id);
     load.play();
   } else {
     width++;
-    document.getElementById('barconfirm').style.width = width + '%'; //ini yang ngatur #barconfirm
-    document.getElementById('percent').innerHTML = width + '%'; //ini yang ngatir percen
+    document.getElementById('barconfirm').style.width = width + '%'; 
+    document.getElementById('percent').innerHTML = width + '%'; 
   }
 }
 window.onload = function () {
@@ -50,7 +42,6 @@ window.onload = function () {
 };
 
 /* ============== HERO ==============  */
-//   ini cuman ada masi intro dari .content dan h1
 gsap.from('.content', {
   duration: 1.5,
   opacity: 0,
@@ -65,8 +56,6 @@ gsap.from('.content h1', {
 
 gsap.from('.content p', { duration: 1.5, y: 50, skewY: 10, opacity: 0 });
 gsap.from('.content .desc', { duration: 1.5, y: 50, skewY: 10, opacity: 0 });
-// gsap.from('.content button', { duration: 1.5, y: 30, ease: 'power4.inOut', opacity: 0 });
-
 gsap.from('.overlay', { y: '100%', ease: 'power4.inOut', duration: 1.2 });
 gsap.fromTo('img', { scale: 1.2 }, { scale: 1, duration: 1, ease: 'power4.inOut' }, '-=.5');
 gsap.from('.button',{y: 200, autoAlpha: 0, duration: 1.5});
